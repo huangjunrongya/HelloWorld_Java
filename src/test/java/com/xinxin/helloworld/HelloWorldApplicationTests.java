afcf6cb5e634e03812028aa55f33a4f358f31822
huanghuang <hh357896ya@gmail.com>
2023-09-05 22:27:40 +0800

    新项目

---
+++ src/test/java/com/xinxin/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinxin.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
