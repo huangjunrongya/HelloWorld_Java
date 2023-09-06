12477b704e6586a01c38dfa5b36f64898db43c19
huanghuang <hh357896ya@gmail.com>
2023-09-06 08:16:25 +0800

    修改包名

--- src/main/java/com/xinxin/HelloWorldApplication.java
+++ src/main/java/com/xinxin/HelloWorldApplication.java
@@ -1,4 +1,4 @@
-package com.xinxin.helloworld;
+package com.xinxin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/xinxin/HelloWorldApplicationTests.java
+++ src/test/java/com/xinxin/HelloWorldApplicationTests.java
@@ -1,4 +1,4 @@
-package com.xinxin.helloworld;
+package com.xinxin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
